5ef779d861b21748a4b1e220e2c312361de20364
manhkhanh03 <khanhmanh0705@gmail.com>
2024-10-26 20:11:00 +0700

    [Feat] update demo

--- src/reads/test.py
+++ src/reads/test.py
@@ -11,10 +11,12 @@ mat = cv2.imread(img_path)
 for i, box in enumerate(boxes):    
     top_left     = (int(box[0][0]), int(box[0][1]))
     bottom_right = (int(box[2][0]), int(box[2][1]))
+    bottom_left  = (int(box[3][0]), int(box[3][1]))
     font = cv2.FONT_ITALIC
     font_scale = 1
     color = (0, 0, 0)
-    position = ((int(box[0][0]) + int(box[3][0])) // 2, (int(box[0][1]) + int(box[3][1])) // 2)
+    position = ((top_left[0] + bottom_left[0]) // 2, (top_left[1] + bottom_left[1]) // 2)
+    
     cv2.rectangle(mat, top_left, bottom_right, (249, 245, 245), -1)
     cv2.putText(mat, texts[i], position, font, font_scale, color, 2)
 

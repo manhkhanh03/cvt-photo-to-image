851fc3444e39e09cc5e8e6a02ed663f1b3b6c736
manhkhanh03 <khanhmanh0705@gmail.com>
2024-11-03 09:36:19 +0700

    [Feat] file test++ supported utf-8 and clean

--- src/reads/test++.py
+++ src/reads/test++.py
@@ -1,9 +1,12 @@
 import easyocr as eo
 import cv2 as cv
+import cv2_ext
 import threading
 import queue
-import time
 import numpy as np
+import time
+from PIL import ImageDraw, Image, ImageFont
+font = ImageFont.truetype(r"C:\Users\khanh\Documents\cvt-photo-to-text\public\fonts\DejaVuSans.ttf", size=20)
 
 class VideoOCR:
     def __init__(self, camera_index=1):
@@ -15,23 +18,24 @@ class VideoOCR:
         self.current_results = []
         self.text_result = np.ones((1000, 800, 3), np.uint8) * 255
         
-    def process_frame(self, frame: np.ndarray) -> np.ndarray:
+    def process_frame(self, frame: np.ndarray) -> None:
         results = self.reader.readtext(frame)
-        if results:
-            self.current_results = []
-            
+        
+        self.current_results = []
+        if results: 
             for (bbox, text, score) in results:
-                if score > 0.2:                     
+                if score > 0.2: 
                     self.current_results.append(text)
-
-    def ocr_thread(self):
+                    
+    def ocr_thread(self) -> None:
         while self.running:
             if not self.frame_contours.empty():
                 frame = self.frame_contours.get()
                 self.process_frame(frame)
-            # time.sleep(0.001)
+                
+            time.sleep(0.1)
             
-    def contours(self):
+    def contours(self) -> None:
         while self.running:
             if not self.frame_queue.empty():
                 frame = self.frame_queue.get()
@@ -41,21 +45,22 @@ class VideoOCR:
                 contours, _ = cv.findContours(laplacian, cv.RETR_EXTERNAL, cv.CHAIN_APPROX_NONE)
                 if contours:
                     max_contour = max(contours, key=cv.contourArea)
-                    x,y,w, h = cv.boundingRect(max_contour)
+                    x, y, w, h = cv.boundingRect(max_contour)
+                    print(f"x: {x}, y: {y}, w: {w}, h: {h}")
                     cropped_img = frame[y:y+h, x:x+w]
-                    cv.imshow('crop img', cropped_img)
-                    self.frame_contours.put(cropped_img)                
-                    key = cv.waitKey(1)
-                    if key == ord('q'): 
+                    cv.imshow('Crop img', cropped_img)
+                    self.frame_contours.put(cropped_img)
+                    if cv.waitKey(1) == ord('q'):
                         self.running = False
                         break
-                    # if not self.frame_contours.full():
+            time.sleep(0.1)
 
-    def run(self, name: str):
-        contours = threading.Thread(target=self.contours)
+    def run(self, name: str) -> None:
+        contours_thread = threading.Thread(target=self.contours)
         ocr_thread = threading.Thread(target=self.ocr_thread)
-        contours.start()
+        contours_thread.start()
         ocr_thread.start()
+        line_step = 20
         
         try:
             while True:
@@ -69,18 +74,16 @@ class VideoOCR:
 
                 if self.current_results:
                     line_start = (200, 200)
-                    line_step = 20
+                    pil_image = Image.fromarray(cv.cvtColor(img_text_result_copy, cv.COLOR_BGR2RGB))
+                    draw = ImageDraw.Draw(pil_image)
                     for text in self.current_results:
-                        font = cv.FONT_HERSHEY_SIMPLEX
-                        font_scale = 0.5
-                        thickness = 1
-                        position = line_start
-            
-                        cv.putText(img_text_result_copy, text, position, font, font_scale, (0, 0, 0), thickness)
-                        cv.imshow("Processed", img_text_result_copy)
+                        draw.text(line_start, text, (0, 0, 0), font, spacing=2)
                         line_start = (line_start[0], line_start[1] + line_step)
+                    
+                    img_text_result_copy = cv.cvtColor(np.array(pil_image), cv.COLOR_RGB2BGR)
+                    cv.imshow("Processed", img_text_result_copy)
                            
-                    cv.imshow(name, frame)
+                cv.imshow(name, frame)
 
                 if cv.waitKey(1) & 0xFF == ord('q'):
                     break
@@ -88,11 +91,10 @@ class VideoOCR:
         finally:
             self.running = False
             ocr_thread.join()
-            contours.join()
+            contours_thread.join()
             self.vid.release()
             cv.destroyAllWindows()
 
 if __name__ == "__main__":
     ocr = VideoOCR(1)
-    name = "Video OCR"
-    ocr.run(name)
+    ocr.run("Video OCR")
